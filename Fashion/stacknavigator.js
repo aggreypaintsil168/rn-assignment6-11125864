e44afc5a5cd00501506e9db669058fb1db815f75
aggreypaintsil168 <ipaggrey@st.ug.edu.gh>
2024-07-09 16:02:50 +0000

    Initial Commit

---
+++ Fashion/stacknavigator.js
@@ -0,0 +1,8 @@
+import {Stylesheet,Text,View} from "react-native"
+import React from "react";
+import Homescreen from "./components/home";
+import CartScreen from "./components/cart";
+
+const Stacknavigator = ()=> {
+    constan
+}
